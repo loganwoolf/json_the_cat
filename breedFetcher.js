a80d788a816fe1ceb844d520abc087e82f9aab40
Logan Woolf <loganwoolf@gmail.com>
2022-01-20 23:20:28 +0000

    Break api call out into function

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,27 +1,25 @@
 const request = require('request');
 
+const endpoint = 'https://api.thecatapi.com/v1/breeds/search?q=';
 
-const breedName = process.argv[2];
-const endpoint = `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`;
+const fetchBreedDescription = (breedName, callback) => {
+  request(endpoint + breedName,
+    (error, response, body) => {
+      if (error) {
+        callback(error);
+        return;
+      }
 
-request(endpoint, (error, response, body) => {
-  if (error) {
-    // console.log(error);
-    console.log(`The call to "${error.hostname}" returned the error: ${error.code}`);
-    error.code === 'ENOTFOUND' && console.log('Check the URL or it may be offline.');
-    process.exit();
-  }
+      const catData = JSON.parse(body)[0];
 
-  const catData = JSON.parse(body)[0];
+      if (!catData) {
+        callback('Error: Invalid breed');
+      }
+      if (catData) {
+        callback(null, `${catData.name}:\n\n ${catData.description}\n`);
+      }
+    }
+  );
+};
 
-  if (!catData) {
-    console.log('Query returned zero results');
-
-  }
-  if (catData) {
-    // const keys = Object.keys(catData);
-    // console.log(`keys:\n${keys}\n\n`);
-    console.log(`${catData.name}:\n\n ${catData.description}\n`);
-  }
-
-});
+module.exports = { fetchBreedDescription };
