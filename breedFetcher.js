e2f4849ee9f21b4b107a8b21e5836d966fbd1755
Logan Woolf <loganwoolf@gmail.com>
2022-01-20 18:12:51 +0000

    Handle empty query

--- breedFetcher.js
+++ breedFetcher.js
@@ -5,9 +5,22 @@ const breedName = process.argv[2];
 const endpoint = `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`;
 
 request(endpoint, (error, response, body) => {
+  if (error) {
+    console.log(`error: ${error}`);
+    console.log(`response : ${response}`);
+    process.exit();
+  }
 
   const catData = JSON.parse(body)[0];
-  const keys = Object.keys(catData);
-  console.log(`keys:\n${keys}\n\n`);
-  console.log(`Description:\n\n ${catData.description}\n`);
+
+  if (!catData) {
+    console.log('Query returned zero results');
+
+  }
+  if (catData) {
+    // const keys = Object.keys(catData);
+    // console.log(`keys:\n${keys}\n\n`);
+    console.log(`${catData.name}:\n\n ${catData.description}\n`);
+  }
+
 });
